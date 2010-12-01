a41afc6aa1e85be5e65beeb011c3a36dd675772a
nilzao <nilzao@gmail.com>
2010-12-01 20:05:54 -0200

    fix css dordserv

--- extensions/dordserv/cabshow.php
+++ extensions/dordserv/cabshow.php
@@ -5,6 +5,8 @@ $htmldiv = "<div id=\"docH\">";
 $helperShowDiv->set_html_Div($htmldiv);
 $vars['d_ord_serv_title;Documento:'] = $dordserv['docSubTipo']->get_descricao();
 $vars['d_ord_servFull_d_ord_serv_cli;Cliente:'] = $dordserv['dordservFull']['d_ord_serv_cli']->get_nome();
+$vars['d_ord_serv_malha;Malha:'] = $dordserv['dordserv']->get_malha();
+$vars['d_ord_serv_fio;Fio:'] = $dordserv['dordserv']->get_fio();
 //$vars['d_cot_compra_data_ini;Data Inicial:'] = $dcotcompra['d_cot_compra']->get_data_ini();
 //$vars['d_cot_compra_data_fim;Data Final:'] = $dcotcompra['d_cot_compra']->get_data_fim();
 $helperShowDiv->monta_Div($vars);
